1d8009976f4e517c8d6ceb72375b4451426ebc2f
Gargi2309 <75556583+Gargi2309@users.noreply.github.com>
2022-01-25 16:03:28 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,64 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h2");
+  }
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+  setElementPosition(){
+    this.titleImage.position(120,160);
+    this.input.position(width/2,height/2);
+    this.playButton.position(width/2,height/2+50);
+    this.greeting.position(width/2,height/2+100);
+  }
+
+  setElementStyle(){
+    this.titleImage.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customBotton");
+    this.greeting.class("greeting");
+
+  }
+
+   handleMousePressed(){
+  this.playButton.mousePressed(()=>{
+    this.input.hide();
+    this.playButton.hide();
+    var message = '
+    hello ${this.input.value()}
+    </br> wait for another player to join... ';
+
+    this.greeting.html(message);
+  })
+
+
+
+  }
+  
+
+
+
+
+
+  display(){
+  this.setElementPosition();
+this.setElementStyle();
+this.handleMousePressed();
+  }
+
+
+
+
+
+
+
+
+
+}
